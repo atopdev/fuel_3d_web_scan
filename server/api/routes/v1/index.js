a08e1a27cf0f6e3104c8590f72626c2e3eac4628
atopdev <25312831+atopdev@users.noreply.github.com>
2018-10-09 14:41:32 +0400

    update the logic of back end

--- server/api/routes/v1/index.js
+++ server/api/routes/v1/index.js
@@ -1,14 +1,10 @@
 const express = require('express');
-const authRoutes = require('./auth.route');
-const userRoutes = require('./user.route');
 const faceRoutes = require('./face.route');
 
 const router = express.Router();
 
 router.get('/status', (req, res) => res.send('OK'));
 
-router.use('/auth', authRoutes);
-router.use('/users', userRoutes);
 router.use('/faces', faceRoutes);
 
 module.exports = router;
--- server/config/index.js
+++ server/config/index.js
@@ -7,11 +7,6 @@ const config = {
   host: process.env.HOST || '0.0.0.0',
   serverHost: process.env.SERVER_HOST || '0.0.0.0',
   port: process.env.SERVER_PORT || 3001,
-  jwtSecret: process.env.JWT_SECRET || 'fuel_3d_face_demo',
-  jwtExpirationInterval: process.env.JWT_EXPIRATION_MINUTES || 15,
-  mongo: {
-    uri: process.env.MONGODB_URI || 'mongodb://localhost:27017/fuel_3d_face_demo',
-  },
   s3: {
     accessKeyId: process.env.S3_ACCESS_KEY_ID,
     secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
